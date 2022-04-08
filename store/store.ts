59c619084b3b1b7d424a0c767e19e13c2af8b18e
Morritz <12800230+Morritz@users.noreply.github.com>
2022-04-08 22:11:03 +0200

    changes to store

--- store/store.ts
+++ store/store.ts
@@ -2,10 +2,11 @@ import create from "zustand";
 import { combine } from "zustand/middleware";
 import { PokemonEntryProps } from "../components/PokemonEntry";
 
-export const useStore = create(
-  combine({ pokemons: [] as PokemonEntryProps[] }, (set) => ({
-    addPokemons: (pokemons: PokemonEntryProps[]) =>
-      set((state) => ({ pokemons: [...state.pokemons, ...pokemons] })),
-    setPokemons: (pokemons: PokemonEntryProps[]) => set({ pokemons: pokemons }),
-  }))
-);
+export const useStore = (pokemons: PokemonEntryProps[]) =>
+  create(
+    combine({ pokemons }, (set) => ({
+      addPokemons: (pokemons: PokemonEntryProps[]) =>
+        set((state) => ({ pokemons: [...state.pokemons, ...pokemons] })),
+      setPokemons: (pokemons: PokemonEntryProps[]) => set({ pokemons }),
+    }))
+  );
