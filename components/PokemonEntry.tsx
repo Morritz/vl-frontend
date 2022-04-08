639e5397a29583027f39edaed3aa6478b549d2d0
Morritz <12800230+Morritz@users.noreply.github.com>
2022-04-08 17:43:43 +0200

    minor ui tweaks

--- components/PokemonEntry.tsx
+++ components/PokemonEntry.tsx
@@ -8,8 +8,8 @@ export interface PokemonEntryProps {
 
 export const PokemonEntry = ({ name, type, sprite }: PokemonEntryProps) => {
   return (
-    <div className="p-2">
-      <div className="flex items-center bg-amber-100 rounded-xl">
+    <div className="p-2 w-[320px]">
+      <div className="flex flex-row items-center bg-yellow-50 rounded-xl p-2 gap-8 drop-shadow">
         <Image src={sprite} width={96} height={96} />
         <div className="flex flex-col">
           <p className="italic text-3xl">{name}</p>
