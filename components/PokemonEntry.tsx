e28a40288d7ba3d99f86353eb586dcfd56974da7
Morritz <12800230+Morritz@users.noreply.github.com>
2022-04-08 17:09:09 +0200

    integrate pokemon fetching on server side with display loic

--- components/PokemonEntry.tsx
+++ components/PokemonEntry.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-interface PokemonEntryProps {
+export interface PokemonEntryProps {
   name: string;
   type: string;
   sprite: string;
@@ -8,8 +8,8 @@ interface PokemonEntryProps {
 
 export const PokemonEntry = ({ name, type, sprite }: PokemonEntryProps) => {
   return (
-    <div className="p-4">
-      <div className="flex items-center bg-amber-100 rounded">
+    <div className="p-2">
+      <div className="flex items-center bg-amber-100 rounded-xl">
         <Image src={sprite} width={96} height={96} />
         <div className="flex flex-col">
           <p className="italic text-3xl">{name}</p>
