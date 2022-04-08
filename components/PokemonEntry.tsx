9b1ef329d0fbed5be0c1fa70def335b5cc4e6091
Morritz <12800230+Morritz@users.noreply.github.com>
2022-04-08 14:46:43 +0200

    simple PokemonEntryComponent

--- components/PokemonEntry.tsx
+++ components/PokemonEntry.tsx
@@ -1,3 +1,21 @@
-export const PokemonEntry = () => {
-  return <div></div>;
+import Image from "next/image";
+
+interface PokemonEntryProps {
+  name: string;
+  type: string;
+  sprite: string;
+}
+
+export const PokemonEntry = ({ name, type, sprite }: PokemonEntryProps) => {
+  return (
+    <div className="p-4">
+      <div className="flex items-center bg-amber-100 rounded">
+        <Image src={sprite} width={96} height={96} />
+        <div className="flex flex-col">
+          <p className="italic text-3xl">{name}</p>
+          <p>Type: {type}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
