6536c21803eae63844a2ca20550267bc21934a1e
Morritz <12800230+Morritz@users.noreply.github.com>
2022-04-09 12:05:32 +0200

    add 5 sec delay to disappear

--- components/Toasts.tsx
+++ components/Toasts.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+
 interface ToastProps {
   message: string;
 }
 const Toast = ({ message }: ToastProps) => {
-  return (
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 5000);
+  }, []);
+  return show ? (
     <div
       id="toast-simple"
       className="flex items-center max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
@@ -10,7 +18,7 @@ const Toast = ({ message }: ToastProps) => {
     >
       <div className="text-sm font-normal">{message}</div>
     </div>
-  );
+  ) : null;
 };
 export const Toasts = () => {
   return (
